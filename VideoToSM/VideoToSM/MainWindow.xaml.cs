7fa1fd02b6fe178d33fff92e2405327e33f26dec
Emanuel-de-Jong <emanueldejong@hotmail.com>
2023-11-21 21:04:59 +0100

    Init

---
+++ VideoToSM/VideoToSM/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace VideoToSM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public VideoReader VideoReader { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            VideoReader.MainLabel = MainLabel;
+            VideoReader = new();
+        }
+
+        private void MainGrid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                MainLabel.Content = files[0];
+
+                VideoReader.Read(files[0]);
+            }
+        }
+    }
+}
---
+++ VideoToSM/VideoToSM/VideoReader.cs
@@ -0,0 +1,42 @@
+﻿using FFmpeg.AutoGen.Abstractions;
+using FFmpeg.AutoGen.Bindings.DynamicallyLoaded;
+using SkiaSharp;
+using System;
+using System.Windows.Controls;
+
+namespace VideoToSM
+{
+    public class VideoReader
+    {
+        public static Label? MainLabel;
+
+        public VideoReader()
+        {
+            DynamicallyLoadedBindings.LibrariesPath = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 24) + "ffmpeg";
+            DynamicallyLoadedBindings.Initialize();
+        }
+
+        public unsafe void Read(string filePath)
+        {
+            using var vsd = new VideoStreamDecoder(filePath);
+
+            var sourceSize = vsd.FrameSize;
+            var sourcePixelFormat = vsd.PixelFormat;
+            var destinationSize = sourceSize;
+            var destinationPixelFormat = AVPixelFormat.@AV_PIX_FMT_BGRA;
+            using var vfc = new VideoFrameConverter(sourceSize, sourcePixelFormat, destinationSize, destinationPixelFormat);
+
+            while (vsd.TryDecodeNextFrame(out var frame))
+            {
+                var convertedFrame = vfc.Convert(frame);
+                var imageInfo = new SKImageInfo(convertedFrame.width, convertedFrame.height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+                using var bitmap = new SKBitmap();
+                bitmap.InstallPixels(imageInfo, (IntPtr)convertedFrame.data[0]);
+
+                var pixel = bitmap.GetPixel(5, 5);
+
+                MainLabel.Content = pixel;
+            }
+        }
+    }
+}
