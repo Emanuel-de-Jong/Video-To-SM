2174727facf0940349f4f13c3f928485d6e32ac8
Emanuel-de-Jong <emanueldejong@hotmail.com>
2023-11-22 09:00:05 +0100

    Note classes

---
+++ VideoToSM/VideoToSM/Enums/ENoteTiming.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoToSM.Enums
+{
+    public enum ENoteTiming
+    {
+        Red,
+        Blue,
+        Yellow,
+    }
+}
---
+++ VideoToSM/VideoToSM/Notes/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoToSM.Enums;
+
+namespace VideoToSM.Notes
+{
+    public class Note
+    {
+        public ENoteTiming noteTiming { get; set; }
+        public decimal Time { get; set; }
+    }
+}
